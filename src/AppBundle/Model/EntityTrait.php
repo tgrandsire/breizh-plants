c5f532bd5d4902c8b1eb59b19b1130d380874f25
Teddy Grandsire <teddy@grandsire.org>
2018-12-02 11:52:23 +0100

    First commit

---
+++ src/AppBundle/Model/EntityTrait.php
@@ -0,0 +1,34 @@
+<?php
+namespace AppBundle\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * Entity trait
+ */
+Trait EntityTrait
+{
+	/**
+	 * Id
+	 *
+	 * @var integer
+	 *
+	 * @ORM\Id
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 *
+	 * @Groups({"game", "play", "turn"})
+	 */
+	protected $id;
+
+	/**
+	 * Gets its id
+	 *
+	 * @return int
+	 */
+	public function getId(): int
+	{
+		return $this->id;
+	}
+}
